22f31388faaeb335048edf9c6edc344b4501ee63
Julien Lange <j.s.lange@kent.ac.uk>
2020-05-05 19:43:31 +0100

    hs files

---
+++ Algo.hs
@@ -0,0 +1,314 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+
+module Algo where
+
+import Parser
+import Automata
+
+import Control.Monad
+import System.Process
+
+import Data.List as L
+import Data.Set as S
+import Data.Map as M
+import Data.Tuple (swap)
+import Data.Either (lefts)
+import Data.Maybe
+import GHC.Generics (Generic)
+import Data.Maybe
+
+-- DEBUG
+import System.IO.Unsafe
+import Debug.Trace
+
+
+type Value = (State, Machine)
+type IValue = (String, Value)
+type Ancestors = Map IValue IValue
+type Edge = (IValue, (Label, IValue))
+type NodeMap = Map IValue String
+data CTree = Node
+             { label :: IValue
+             , children :: [(Label, CTree)]
+             , status :: NType
+             }
+           deriving (Show, Eq)
+
+data NType = Increase | Decrease | Seen | Interm | Bound
+           deriving (Show, Eq)
+     
+checkingAlgorithm :: Int -> Bool -> Bool -> Bool -> LocalType -> LocalType -> IO ()
+checkingAlgorithm bound dual debug nomin t1 t2 =
+  let fm = if dual then dualMachine else id
+      (m1, m2) = (if dual then swap else id) $ (fm $type2Machine nomin "-" t1, fm $ type2Machine nomin "+" t2)
+  in do when debug $
+          do machine2file m1 "sub"
+             machine2file m2 "sup"
+    
+        case buildTree bound debug m1 m2 of 
+          Nothing -> putStrLn "Maybe"
+          Just (b,(t,ancs)) -> do let ts = splitTree ancs t                                      
+                                  putStrLn (show $ b && (L.all (goodTree ancs) ts))
+                                  when debug $ 
+                                    do let 
+                                           
+                                       putStrLn $ "Candidate sub-type controllable: "++(show $ isControllable m1)
+                                       putStrLn $ "Candidate super-type controllable: "++(show $ isControllable m2)
+                                       putStrLn $ "Candidate sub-type strong controllable: "++(show $ isStrongControllable m1)
+                                       putStrLn $ "Candidate super-type strong controllable: "++(show $ isStrongControllable m2)
+                                       
+                                       writeToFile "sim_tree.dot" (printTrees ancs [t])
+                                       mkPicture ("sim_tree.dot") ("sim_tree.svg")
+
+                                       writeToFile "witness_trees.dot" (printTrees ancs ts)
+                                       mkPicture ("witness_trees.dot") ("witness_trees.svg")
+    
+               
+buildTree :: Int -> Bool -> Machine -> Machine -> Maybe (Bool, (CTree, Ancestors))
+buildTree bound debug m1 m2 = helper bound ("0", (tinit m1, m2)) [] 
+
+  where helper :: Int -> IValue -> [(Label, IValue)] -> Maybe (Bool, (CTree, Ancestors))
+        helper b v seen 
+          | b == 0 = Just (False, ((Node v [] Bound), M.empty))
+          | otherwise = case find (equalConf debug v) (L.map snd seen) of
+            -- increase
+            Just anc -> Just (True, ((Node v [] Increase), M.singleton v anc))
+            -- Nothing -> case find (\x -> equalConf debug x v) (L.map snd seen) of
+            --   -- decrease
+            --   Just anc -> Just (True, ((Node v [] Decrease), M.singleton v anc))
+            Nothing -> case oneStep debug m1 (snd v) of
+                -- continue
+                Just ys -> continuation b v (nextconfs ys (fst v)) seen
+                Nothing -> Just (False, ((Node v [] Interm), M.empty))
+              
+        nextconfs :: [(Label, Value)] -> String -> [(Label, IValue)]
+        nextconfs xs i = snd $ mapAccumL (\x y -> (x+1, (fst y, (i++(show x), snd y )))) 0 xs 
+        
+        continuation :: Int -> IValue -> [(Label, IValue)] -> [(Label, IValue)] -> Maybe (Bool, (CTree, Ancestors))
+        continuation b v xs seen = case sequencePair $ L.map (\(a,x) -> (a, helper (b-1) x ((a,v):seen))) xs of
+          Nothing -> Nothing
+          -- ys  :: (Label, (Bool, (CTree, Ancestors)))
+          Just ys -> let zs = L.map (\(l,(b,(t,ancs))) -> (l,t)) ys 
+                         mps = L.map (\(l,(b,(t,ancs))) -> ancs) ys
+                         res = and $ L.map (\(l,(b,(t,ancs))) -> b) ys
+                     in Just (res, ((Node v zs Interm), M.unions mps))
+                        
+                        
+splitTree :: Ancestors -> CTree -> [CTree]
+splitTree ancs t = helper ancs t
+  where helper acs (Node v xs f) 
+          | v `L.elem` acs = [Node v xs f]
+          | otherwise = concat $ L.map (\(x,c) -> helper acs c) xs
+                        
+goodTree :: Ancestors -> CTree -> Bool
+goodTree ancs t@(Node v xs f) = L.all (\n -> equalConf False n v) $ L.intersect (leaves t) descendants
+  where descendants =  L.map fst $ L.filter (\((i,(s,m)),(j,(s',m'))) -> (not $ bisimilar m m')) $ M.toList ancs
+        leaves (Node v [] f) = [v]
+        leaves (Node v xs f) = concat $ L.map (leaves . snd) xs
+
+sequencePair :: [(a, Maybe b)] -> Maybe [(a,b)]
+sequencePair [] = Just [] 
+sequencePair ((x,Just y):xs) = case sequencePair xs of
+  Just ys -> Just ((x,y):ys)
+  Nothing -> Nothing
+sequencePair ((x,Nothing):xs) = Nothing
+
+equalConf :: Bool -> IValue -> IValue -> Bool
+equalConf debug (i1, (p,m)) (i2, (p',m')) = (p==p') && (equivRepeat debug m' m) 
+          
+equivRepeat :: Bool -> Machine -> Machine -> Bool
+equivRepeat debug m1 m2 = (helper (tinit m1, tinit m2) [])
+  where helper (p,q) seen 
+          | (p,q) `L.elem` seen = True
+          | otherwise = 
+            (bisimilar (updateInit p m1) (updateInit q m2))
+            ||
+            (              
+              (nonempty p)
+              &&
+              (nonempty q)
+              &&
+              ((ssucc . removess $ p) == (removess q))
+              &&
+              (
+                let pmoves = successors m1 p
+                    qmoves = successors m2 q
+                in ((S.fromList (L.map fst pmoves)) == (S.fromList (L.map fst qmoves)))
+                   && helper (p,unpeel q) ((p,q):seen)
+              )
+            )
+        removess xs = L.filter ('s'/=) xs
+
+ssucc :: State -> State
+ssucc s = "c"++s 
+
+nonempty :: State -> Bool
+nonempty ('c':xs) = True
+nonempty _ = False
+
+unpeel :: State -> State
+unpeel ('c':x:xs)  
+  | (x /= 'c') = 's':x:xs 
+  | otherwise = 'c':(unpeel (x:xs))
+unpeel ('s':xs) = 's':xs
+unpeel [] = []
+
+osucc :: State -> State
+osucc s = "s"++s
+
+isFinalConf :: Machine -> Value -> Bool
+isFinalConf m1 (p, m) = (isFinal m1 p) && (isFinal m (tinit m))
+          
+
+oneStep :: Bool -> Machine -> Value -> Maybe [(Label, Value)]
+oneStep debug m1 v@(p,m)
+  | isFinalConf m1 v = (if debug then (trace ("Final: "++(show (p,(tinit m)))++"\n"++(printMachine m)) ) else (\x  -> x  )) $ 
+    Just []
+    --
+  | not $ isControllable m = Just [] 
+    --
+  | (isInput m1 p) && (isInput m (tinit m)) && ((inBarb m1 p) == (inBarb m (tinit m))) =  
+      (if debug then (trace ("In: "++(show (p,(tinit m)))) ) else (\x  -> x  )) $ 
+      let  psmoves = L.map snd $ L.filter (\(x,(y,z)) -> x==p) $ transitions m1
+           qsmoves = L.map snd $ L.filter (\(x,(y,z)) -> x==(tinit m)) $ transitions m               
+           next = L.nub $ [(a,(x, cleanUp $ updateInit y m)) | (a,x) <- psmoves, (b,y) <- qsmoves, a==b]
+      in Just next
+         --
+  | (isOutput m1 p) && (isOutput m (tinit m)) && ((outBarb m1 p) == (outBarb m (tinit m))) = 
+        (if debug then (trace ("OutSync: "++(show (p,(tinit m)))++"\n"++(printMachine m)) ) else (\x  -> x  )) $ 
+        let psmoves = L.map snd $ L.filter (\(x,(y,z)) -> x==p) $ transitions m1
+            qsmoves = L.map snd $ L.filter (\(x,(y,z)) -> x==(tinit m)) $ transitions m               
+            next = L.nub $ [(b,(x,cleanUp $ updateInit y m)) | (a,x) <- psmoves, (b,y) <- qsmoves, a==b]
+        in Just next
+  | (isOutput m1 p) && not (isOutput m (tinit m)) =
+          (if debug then (trace ("Out: "++(show (p,(tinit m)))++"\n"++(printMachine m)) ) else (\x  -> x  )) $ 
+          let psmoves = L.map snd $ L.filter (\(x,(y,z)) -> x==p) $ transitions m1              
+              qstates = reachableSendStates (tinit m) m
+              newmachines = L.map (\a -> ((Send, a), replaceInMachine m (Send, a) qstates)) $ S.toList (outBarb m1 p)
+              next = L.nub $ [(a, (x, updateInit (ssucc (tinit m)) m'))| (a,x) <- psmoves, (b,m') <- newmachines, a==b] 
+          in if (not $ L.null qstates) 
+                &&
+                (L.and $ L.map (\x -> (outBarb m1 p) == (outBarb m x)) qstates)
+             then Just next
+             else (if debug then (trace ("BadOut: "++(show (p,(tinit m)))++"\n"++(printMachine m)) ) else (\x  -> x  )) $ 
+                  Nothing
+  | otherwise = (if debug then (trace ("Bad: "++(show (p,(tinit m)))++"\n"++(printMachine m)) ) else (\x  -> x  )) $ 
+                Nothing
+                      
+
+
+
+replaceInMachine :: Machine -> Label -> [State] -> Machine
+replaceInMachine m l qs = cleanUp $ Machine { states = nstates
+                                            , tinit = ssucc $ tinit m
+                                            , transitions = nedges
+                                            , accepts = nstates
+                                            }
+  where alltrans = nub $ transitions m
+        nstates = nub $ concat $ L.map (\(s,(a,t)) -> [s,t]) nedges
+        nedges = (normalisedEdges prefix) `L.union` (normalisedEdges suffix)
+        prefix = (setRight (alltrans L.\\ filtered)) `L.union` replaced
+        setRight xs = L.map (\(s,(a,t)) -> (Right s,(a, Right t))) xs
+        suffix = L.map (\(s,(a,t)) -> (Left s,(a,Left t))) alltrans
+        normalise (Right s) = ssucc s
+        normalise (Left s) = osucc s
+        normalisedEdges xs =  L.map (\(s,(a,t)) -> (normalise s,(a, normalise t))) xs
+        replaced = [(Right s1, (l1, Left t2)) | (s1,(l1,t1)) <- alltrans
+                                              , (s2,(l2,t2)) <- alltrans
+                                              , t1==s2
+                                              , s2 `elem` qs 
+                                              , l2 == l
+                                              , isReceiveLab l1
+                                              ]
+        filtered = [(s1,(l1,t1)) | (s1,(l1,t1)) <- alltrans , t1 `elem` qs]
+ 
+        
+
+
+
+-- --------------------- PRINTING STUFF -----------------
+
+printNodeLabel :: IValue -> String
+printNodeLabel (i,(p,m)) = 
+  let nm = rename (\s -> i++s) m
+  in "subgraph cluster_conf"++i++" {\n"
+     ++"label = \""++i++": "++p++"\";\n"
+     ++(printMachineBody nm)
+     ++"}"
+
+mkNodeMap :: Int -> [IValue] -> NodeMap
+mkNodeMap i xs = M.fromList $ L.map (\(id,y) -> ((id,y),"node"++(show i)++id)) xs
+
+printNodeId :: NodeMap -> IValue -> String
+printNodeId  map v = case M.lookup v map of
+                      Just s -> s
+                      Nothing -> error $ "Node "++(show v)++" not found."
+
+printNode :: NodeMap -> IValue -> String
+printNode map v@(i,(p,m)) = 
+  let nm = rename (\s -> i++s) m
+  in "subgraph cluster_conf"++(printNodeId map v)++" {\n"
+     ++"label = \" L"++i++": "++p++"\";\n"
+     ++(printNodeId map v)++"[style = invis];\n" -- style = invis
+     ++(printMachineBody nm)
+     ++"}"
+
+mkGraph :: CTree -> (IValue, [Edge])
+mkGraph (Node v xs _) = (v, helper v xs)
+  where helper n ys = concat $
+                          L.map (\(l, (Node w zs _)) -> (n,(l, w)):(helper w zs)) ys
+
+
+printEdge :: NodeMap -> Edge -> String
+printEdge map (s,(l,t)) = (printNodeId map s)++" -> "++(printNodeId map t)
+                          ++"[lhead=cluster_conf"++(printNodeId map t)
+                          ++", ltail=cluster_conf"++(printNodeId map s)
+                          ++", label=\""++(printEdgeLabel l)++"\", minlen=3];"
+
+printAncestorEdge :: NodeMap -> (IValue, IValue) -> String
+printAncestorEdge map (s,t) = (printNodeId map s)++" -> "++(printNodeId map t)
+                              ++" [lhead=cluster_conf"++(printNodeId map t)
+                              ++", ltail=cluster_conf"++(printNodeId map s)
+                              ++", minlen=3, style=dashed];"
+
+
+
+printGraph :: Int -> IValue -> [Edge] -> Ancestors -> String
+printGraph i init edges ancestors =
+  let nodes = nub $ concat $ L.map (\(s,(l,t)) -> [s,t]) edges
+      nmap = mkNodeMap i nodes
+      snodes = intercalate "\n" $ L.map (printNode nmap) nodes
+      sedges = intercalate "\n" $ L.map (printEdge nmap) edges
+      ancs = intercalate "\n" $ L.map (printAncestorEdge nmap) $
+             L.filter (\(t,s) -> (t `L.elem` nodes) && (s `L.elem` nodes)) $
+             M.toList ancestors
+  in snodes++"\n"
+     ++sedges++"\n"
+     ++ancs++"\n"
+
+printTree :: Int -> Ancestors -> CTree -> String
+printTree i ancs t = let (init, edges) = mkGraph t
+                     in printGraph i init edges ancs
+
+printTrees :: Ancestors -> [CTree] -> String
+printTrees ancs list =  "digraph CTs { \n graph [fontsize=10 fontname=\"Verdana\" compound=true]; \n node [shape=record fontsize=10 fontname=\"Verdana\"]; \n rankdir = TD ranksep = 1.2 nodesep = 0.5; \n"++(helper $ snd $ mapAccumL (\x y -> (x+1, (x,y))) 0 list)++"}\n"
+  where helper ((i,x):xs) = 
+          let header = "subgraph cluster_"++(show i)
+                       ++" {\n"
+                       ++ "label = \""++(show i)++"\";\n"      
+              footer = "}\n"
+              tr = printTree i ancs x
+          in (header++tr++footer)++"\n"++(helper xs)
+        helper [] = ""
+              
+                           
+
+printEdgeLabel :: Label -> String
+printEdgeLabel (Send, msg) = "!"++(msg)
+printEdgeLabel (Receive, msg) = "?"++(msg)
+
+
+
+
+              
---
+++ Checker.hs
@@ -0,0 +1,98 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
+import Parser
+
+-- import GayAndHole (mkGHsubtyping)
+-- import BLZsubtyping (mkLBZsubtyping)
+-- import BLocalType (mksubtyping)
+
+import Algo (checkingAlgorithm)
+
+import Data.List as L
+import Data.Map as M
+import Data.Set as S
+import Data.Tree
+import System.Environment
+import System.FilePath.Posix
+import System.Process
+import System.Console.CmdArgs
+import GHC.IO.Handle
+import Control.Monad
+import Data.Text (strip, pack, unpack)
+import Data.Time.Clock (UTCTime, getCurrentTime, diffUTCTime)
+import Text.Printf (printf)
+
+-- DEBUG
+import System.IO.Unsafe
+import Debug.Trace
+
+writeToFile :: FilePath -> String -> IO()
+writeToFile file content = writeFile file content
+
+data Interaction = Passive 
+                 | Interactive
+                 deriving (Data,Typeable,Show,Eq)
+
+data SubtypingMode = BLZ
+                   deriving (Data,Typeable,Show,Eq)
+
+data Subtyping = Subtyping 
+                 { interaction :: Interaction
+                 , typingmode :: SubtypingMode
+                 , sub :: String
+                 , sup :: String
+                 , debug :: Bool 
+                 , bound :: Int
+                 , dualflag :: Bool
+                 }
+               deriving (Data,Typeable,Show,Eq)
+submodes =  enum
+           [ BLZ  &= help "BLZ subtyping relation (default, no need to specify)" &= name "G"
+           ]
+
+
+subargs = Subtyping 
+ { interaction = enum [ Passive &= help "Passive mode (arguments are paths to files)"
+                      , Interactive &= help "Interactive mode (arguments are given in line)"
+                      ]
+ , typingmode = submodes
+ , sub = def  &= argPos 0  &= typ "FILE/LOCALTYPE" 
+ , sup = def &= argPos 1  &= typ "FILE/LOCALTYPE"
+ , bound = def &= opt "-1"  &= argPos 2  &= typ "INT"
+ , debug = def
+           &= explicit &= name "debug"
+           &= help "Print debug info and pictures (png)"
+ , dualflag = def 
+              &= explicit &= name "dual"
+              &= help "tests for dual(m1) > dual(m2)"
+ }  &= help "Session type subtyping relations as model checking problems"
+
+
+
+getLocalTypeString :: Interaction -> String -> IO String
+getLocalTypeString Passive s = readFile s
+getLocalTypeString Interactive s = return s
+
+
+main :: IO ()
+main = do 
+  pargs <- cmdArgs (modes [subargs]) 
+  subinp <- getLocalTypeString (interaction pargs) (sub pargs)
+  supinp <- getLocalTypeString (interaction pargs) (sup pargs)
+  case parseLocalType subinp of
+    Left err -> print err
+    Right subans -> 
+      case parseLocalType supinp of
+        Left err -> print err
+        Right supans ->
+          if not (wellFormed subans && wellFormed supans)
+          then putStrLn "Error in local type (not well-formed)."
+          else 
+            do
+              start <- getCurrentTime
+              checkingAlgorithm (bound pargs) (dualflag pargs) (debug pargs) True subans supans
+              end <- getCurrentTime
+              putStrLn $ (show $ diffUTCTime end start)
+            --  return ()
+  
+ 

11f202f785b9bc2d568331794bad6c9076afaa52
Julien Lange <julien.lange@rhul.ac.uk>
2023-05-17 12:45:45 +0100

    add top axis

--- scripts/mkplot.py
+++ scripts/mkplot.py
@@ -10,128 +10,91 @@ import os
 from matplotlib.ticker import ScalarFormatter 
 from scipy.optimize import curve_fit
 
-filetype = ".eps"
+filetype = ".jpeg"
 ticksfontsize = 12
 axisfontsize = 15
 legentfontsize = 15
-mymarkersize=5
+mymarkersize = 5
 
 
-def fitfunc(x, a, b, c ):
+def fitfunc(x, a, b, c):
     return a * np.power(b, x) + c
 
 
-def mkFitLegend(ca,cb,cc):
-    return (r'$F(x)\approx%5.5f + %5.5f * %5.4f^x$' % (cc,ca,cb))
+def mkFitLegend(ca, cb, cc):
+    return (r'$F(x)\approx%5.5f + %5.5f * %5.4f^x$' % (cc, ca, cb))
 
-def mkPlot(bfile,outpath):
-    tab = np.loadtxt(bfile,
-                         usecols=(3,1,4),
-                         unpack = True,
-                         delimiter = ',',
-                         dtype = float
-                         )
+
+def mkPlot(bfile, outpath):
+    tab = np.loadtxt(
+        bfile,
+        usecols=(3, 1, 4, 0),
+        unpack=True,
+        delimiter=',',
+        dtype=float
+    )
 
     stab = tab[0].argsort()
-    final = tab[:,stab]
-    tr,tm,tk = final
-    # maxtr = max(tr)
+    final = tab[:, stab]
+    tr, tm, tk, tp = final
 
-    
-    plt.figure(figsize=(9, 9))    
-
-    fix, ax = plt.subplots()
-
+    plt.figure(figsize=(9, 12))
+    fig, ax1 = plt.subplots()
 
     linx = np.array(tr)
     liny = np.array(tk)
-    linspace = np.linspace(0,max(tr))
-    logspace = np.logspace(0,max(tr))
-    # popt, pcov = curve_fit(fitfunc, linx, liny, bounds=(0, [100, 10, 20]))
-    # popt, pcov = curve_fit(fitfunc, linx, liny, bounds=(0, [100, 10, 20]))
-    popt, pcov = curve_fit(fitfunc, linx, liny,maxfev=8000)
+    linspace = np.linspace(0, max(tr))
+    logspace = np.logspace(0, max(tr))
+    popt, pcov = curve_fit(fitfunc, linx, liny, maxfev=8000)
 
-    print("Fitted curve: "+str(popt))
-    # print(pcov)
+    print("Fitted curve: " + str(popt))
 
-
-    preflegend = "Execution time" # "Time with "
+    preflegend = "Execution time"
     memlegend = "Memory usage"
-    legend = "" # "NA"
-
 
     plt.yticks(fontsize=ticksfontsize)
     plt.xticks(rotation=40, ha='right', fontsize=ticksfontsize)
-  
-    plt.grid( linestyle='dotted', color='gray')
 
-    ax.set_ylabel('Time (seconds)', fontsize=axisfontsize)
+    plt.grid(linestyle='dotted', color='gray')
 
-    # plt.yscale('lin')
-    
-    ax2 = ax.twinx()
-    # ax2.set_yscale("lin")
-
-    
-    ax.plot(linspace, fitfunc(linspace, *popt),color='orange',zorder=10)  #, linestyle='None',marker='.') 
-    ax.plot(tr,tk,marker='o',markersize=mymarkersize,linestyle='None',color='blue',zorder=20)
-    ax2.plot(tr, tm,marker='.',markersize=mymarkersize,linestyle='None',color='red',zorder=15)
-
-    
+    ax1.set_ylabel('Time (seconds)', fontsize=axisfontsize)
+    ax2 = ax1.twinx()
     ax2.set_ylabel('Memory (bytes)', fontsize=axisfontsize)
+    ax3 = ax1.twiny()
 
-    ax2.legend([memlegend],loc=(0.05,0.7),fontsize=legentfontsize,numpoints=1)
+    ax1.plot(linspace, fitfunc(linspace, *popt), color='orange', zorder=10)
+    ax1.plot(tr, tk, marker='o', markersize=mymarkersize, linestyle='None', color='blue', zorder=20)
+    ax2.plot(tr, tm, marker='.', markersize=mymarkersize, linestyle='None', color='red', zorder=15)
 
-    
-    
-    (ca,cb,cc) = tuple(popt)
-    ax.legend(
-        [mkFitLegend(ca,cb,cc),
-         preflegend+legend,memlegend], loc=(0.05,0.8),fontsize=legentfontsize,numpoints=1)
+    ax1.legend([mkFitLegend(*popt), preflegend, memlegend], loc=(0.05, 0.8), fontsize=legentfontsize, numpoints=1)
+    ax2.legend([memlegend], loc=(0.05, 0.7), fontsize=legentfontsize, numpoints=1)
 
-    # ax.legend([preflegend+legend,memlegend], loc=(0.05,0.9), fontsize=legentfontsize)
+    ax1.set_xlabel(r'Number of transitions in candidate supertype', fontsize=axisfontsize)
 
-    
+    ax3.set_xlim(ax1.get_xlim())
+    ax3.set_xticks(tr)
+    ax3.set_xticklabels(tp.astype(int))
+    ax3.set_xlabel(r"Parameter")
 
-    # xlabel = "m"
-    # if bfile.find("_A_") > 1:
-    #     xlabel = "n"
-    # if bfile.find("_C_") > 1:
-    #     xlabel = "k"
-    # ax.set_xlabel( r'Parameter $'+xlabel+'$',fontsize=axisfontsize)
-    ax.set_xlabel( r'Number of transitions in candidate supertype',fontsize=axisfontsize)
-    
-    plt.xscale('linear')
+    posfifx = '-lin' + filetype
+    # plt.ticklabel_format(style='sci', axis='x', useOffset=True)
 
-
-    posfifx = '-lin'+filetype
-    plt.ticklabel_format(style='sci', axis='x',useOffset=True)
-
-
-
-    plt.savefig('./plots/'+outpath #bfile.replace(".","-")+posfifx
-                    , dpi=300
-                    , bbox_inches="tight")
-                    # , frameone=False)
-    
-
-    # plt.show()
+    plt.savefig('./plots/' + outpath, dpi=300, bbox_inches="tight")
 
 
 if not os.path.exists('./plots'):
     os.makedirs('./plots')
 
-
 datadir = "./"
-if (len(sys.argv) > 1):
+if len(sys.argv) > 1:
     datadir = sys.argv[1]
-    
+
 print("Source data:", datadir)
 
 i = 0
 for f in os.listdir(datadir):
-    if (f.startswith("parametrised-benchmarks_")) and (f.endswith(".csv")):
-        ostr = f+"plot-"+string.ascii_lowercase[i]+filetype
-        print("Converting "+f+" to "+ostr)
-        mkPlot(os.path.abspath(datadir+f),ostr)
+    if f.startswith("parametrised-benchmarks_") and f.endswith(".csv"):
+        ostr = f + "plot-" + string.ascii_lowercase[i] + filetype
+        print("Converting " + f + " to " + ostr)
+        mkPlot(os.path.abspath(datadir + f), ostr)
         i += 1
